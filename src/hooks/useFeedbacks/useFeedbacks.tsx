0d629b750de649c0ba670a7891fb52f925c24ecb
daysilva <adaiane73@gmail.com>
2023-07-26 14:55:32 -0300

    atualizaçao

--- src/hooks/useFeedbacks/useFeedbacks.tsx
+++ src/hooks/useFeedbacks/useFeedbacks.tsx
@@ -3,6 +3,13 @@ import api from "../../services/api";
 import { IPublicacao } from "..";
 import { AuthContext } from "../../contexts";
 
+
+export interface IFeedback {
+  idFeedback: number;
+  name: string;
+  address?: string;
+  type: "Local" | "Categoria";
+}
 export interface ILocal {
   idLocal?: number;
   name: string;
@@ -10,6 +17,8 @@ export interface ILocal {
   publicacoes?: IPublicacao[];
 }
 
+
+  
 const useFeedbacks = () => {
   async function createFeedback({ name, address }: ILocal) {
     try {
@@ -24,7 +33,58 @@ const useFeedbacks = () => {
     }
   }
 
-  return { createFeedback };
+
+
+  async function getFeedbacks() {
+    try {
+      const response = await api.get("/Feedback");
+      return response.data;
+    } catch (e) {
+      throw new Error("Algo deu errado ao listar os feedbacks. " + e);
+    }
+  }
+
+  async function createFeedbackLocal({ name, address }: IFeedback) {
+    try {
+      const request = await api.post("/Feedback", {
+        name,
+        address,
+        type: 'Local',
+      });
+
+      return request.data;
+    } catch (e) {
+      throw new Error("Algo deu errado ao criar um feedback de local. " + e);
+    }
+  }
+
+  async function createFeedbackCategoria({ name }: IFeedback) {
+    try {
+      const request = await api.post("/Feedback", {
+        name,
+        type: 'Categoria',
+      });
+
+      return request.data;
+    } catch (e) {
+      throw new Error("Algo deu errado ao criar um feedback de categoria. " + e);
+    }
+  }
+
+  async function confirmFeedback(idFeedback: number) {
+    try {
+      const response = await api.post(
+        `/Feedback/ConfirmFeedback/${idFeedback}`,
+        {},
+      );
+
+      return response.data;
+    } catch (e) {
+      throw new Error("Algo deu errado ao confirmar o feedback. " + e);
+    }
+  }
+  return {createFeedback, createFeedbackLocal, createFeedbackCategoria, confirmFeedback, getFeedbacks };
+
 };
 
 export default useFeedbacks;
